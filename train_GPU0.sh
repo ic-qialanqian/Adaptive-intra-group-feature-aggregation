77f8b6e12fc27de0c3326e817dc787acef777ee5
ic-qialanqian <58182792+ic-qialanqian@users.noreply.github.com>
2023-02-15 11:54:03 +0000

    Update train_GPU0.sh

--- train_GPU0.sh
+++ train_GPU0.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --model GCoNet --loss DSLoss_IoU_noCAM --trainset DUTS_class --size 224 --tmp tmp/sizes_statistics --lr 1e-4 --bs 16 --epochs 1
+CUDA_VISIBLE_DEVICES=0 python train.py --model AIGANet --loss DSLoss_IoU_noCAM --trainset DUTS_class --size 224 --tmp tmp/sizes_statistics --lr 1e-4 --bs 16 --epochs 1
