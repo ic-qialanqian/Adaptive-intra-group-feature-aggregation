5109890697d6b903973769523e5eef7c4a10ce55
ic-qialanqian <58182792+ic-qialanqian@users.noreply.github.com>
2023-02-15 11:56:22 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --model GCoNet --param_root tmp/dot_product --save_root ./data/dot_product
-CUDA_VISIBLE_DEVICES=0 python eval-co-sod/main.py --pred_dir ./data/dot_product --gt_dir ./data/gts
+CUDA_VISIBLE_DEVICES=0 python test.py --model AIGANet --param_root tmp/AIGANet --save_root ./data/AIGANet
+CUDA_VISIBLE_DEVICES=0 python eval-co-sod/main.py --pred_dir ./data/AIGANet --gt_dir ./data/gts
